c39c7fce685e51e4d87d3ceac4fdc2fbee0cf898
Adel Napier <adelnapier@MacBook-Air.local>
2025-01-08 12:07:10 +0500

    Удалены файлы из индекса, добавлен .gitignore

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -6,6 +6,12 @@ def main():
     )
     parser.add_argument("first_file", help="Path to the first file")
     parser.add_argument("second_file", help="Path to the second file")
+    parser.add_argument(
+    	"-f", "--format",
+    	help="set format of output",
+    	dest="format",
+    	default="stylish"
+	)
     args = parser.parse_args()
 
 if __name__ == "__main__":
